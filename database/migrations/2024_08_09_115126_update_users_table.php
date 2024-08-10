d552c20f55a38a581f2002296eb7ae0e94f6281a
Salar Haider <salarhaider05@gmail.com>
2024-08-10 12:31:01 +0500

    commit files

--- database/migrations/2024_08_09_115126_update_users_table.php
+++ database/migrations/2024_08_09_115126_update_users_table.php
@@ -11,9 +11,8 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('users', function (Blueprint $table) {
-            $table->id();
-            $table->timestamps();
+        Schema::table('users', function (Blueprint $table) {
+            
         });
     }
 
@@ -22,6 +21,8 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('users');
+        Schema::table('users', function (Blueprint $table) {
+            //
+        });
     }
 };
